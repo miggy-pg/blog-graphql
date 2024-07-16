db445775260704397a59aefad4ada3a7a2871fc8
miggy-pg <meelan1323@gmail.com>
2024-07-17 03:36:29 +0800

    wip: fix node to serve other routes

--- client/components/PostCreate/index.js
+++ client/components/PostCreate/index.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { useForm } from "react-hook-form";
 
 function PostCreate() {
+  const { register, handleSubmit, reset } = useForm();
+
   return (
-    <div>PostCreate</div>
-  )
+    <div>
+      PostCreate
+      <form>
+        <input {...register("title")} />
+        <input {...register("")} />
+      </form>
+    </div>
+  );
 }
 
-export default PostCreate
+export default PostCreate;
--- server/server.js
+++ server/server.js
@@ -1,43 +1,44 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express');
-const models = require('./models');
-const expressGraphQL = require('express-graphql').graphqlHTTP
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const schema = require('./schema/schema');
+const express = require("express");
+const models = require("./models");
+const expressGraphQL = require("express-graphql").graphqlHTTP;
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const schema = require("./schema/schema");
 const app = express();
-const path = require("path")
+const path = require("path");
 
 const MONGO_URI = process.env.MONGO_URI;
 if (!MONGO_URI) {
-  throw new Error('You must provide a Mongo Atlas URI');
+  throw new Error("You must provide a Mongo Atlas URI");
 }
 
 mongoose.Promise = global.Promise;
 mongoose.connect(MONGO_URI);
 mongoose.connection
-  .once('open', () => console.log('Connected to Mongo Atlas instance.'))
-  .on('error', (error) =>
-    console.log('Error connecting to Mongo Atlas:', error)
+  .once("open", () => console.log("Connected to Mongo Atlas instance."))
+  .on("error", (error) =>
+    console.log("Error connecting to Mongo Atlas:", error)
   );
 
 app.use(bodyParser.json());
 app.use(
-  '/graphql',
+  "/graphql",
   expressGraphQL({
     schema,
-    graphiql: true
+    graphiql: true,
   })
 );
 
-const test = path.join(__dirname, 'client/build')
-console.log("test: ", test);
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, "client/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
 
-const webpackMiddleware = require('webpack-dev-middleware');
-const webpack = require('webpack');
-const webpackConfig = require('../webpack.config.js');
+const webpackMiddleware = require("webpack-dev-middleware");
+const webpack = require("webpack");
+const webpackConfig = require("../webpack.config.js");
 app.use(webpackMiddleware(webpack(webpackConfig)));
 
 module.exports = app;
